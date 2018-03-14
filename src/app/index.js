1de714d3b11c04aa36cd65c7727e65a3abbac926
vrkaushik <vrkaushik20@gmail.com>
2018-03-14 20:05:22 +0530

    added reactjs app files

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,17 @@
-console.log("It works!");
+
+import React from "react";
+import { render } from "react-dom";
+class App extends React.Component
+{
+ render(){
+  return(
+    <div>
+        <h1>Hello</h1>
+    </div>
+   ) ;
+
+
+ }
+}
+
+render(<App/>,window.document.getElementById("app"));
